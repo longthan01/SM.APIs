97f4418f041447cf3d6c92ac5ac1be41b1110939
SM\SM <temp.smalldragon@gmail.com>
2020-11-24 16:59:24 +0700

    Add seed data and initial setup for SQL server

--- SM.APIs.AuthServer/Config.cs
+++ SM.APIs.AuthServer/Config.cs
@@ -11,16 +11,67 @@ namespace SM.APIs.AuthServer
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
-            { 
+            {
                 new IdentityResources.OpenId()
             };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            { };
 
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
+            new Client[]
+            { 
+            // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "webclient",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedScopes = { "full" }
+                },
+
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "interactive",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile",}
+                },
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { 
+            // invoice API specific scopes
+                new ApiScope(name: "story.read",   displayName: "Reads stories."),
+
+                // customer API specific scopes
+                new ApiScope(name: "story_category.read",    displayName: "Reads story's category."),
+
+                // shared scope
+                new ApiScope(name: "manage", displayName: "Provides administrative access to story and category data.")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+             new List<ApiResource>
+             {
+                 new ApiResource("story", "Story API")
+                 {
+                     Scopes = { "story.read", "invoice.pay", "manage" }
+                 },
+
+                 new ApiResource("story_category", "Story's category API")
+                 {
+                     Scopes = { "story_category.read", "manage" }
+                 }
+             };
     }
 }
--- SM.APIs.AuthServer/Startup.cs
+++ SM.APIs.AuthServer/Startup.cs
@@ -6,31 +6,52 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace SM.APIs.AuthServer
 {
     public class Startup
     {
         public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
 
-        public Startup(IWebHostEnvironment environment)
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
             Environment = environment;
+            Configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
             // uncomment, if you want to add an MVC-based UI
             //services.AddControllersWithViews();
+            string connectionString = Configuration.GetConnectionString("DefaultConnectionString");
+            var migrationsAssembly = typeof(SeedData).GetTypeInfo().Assembly.GetName().Name;
 
             var builder = services.AddIdentityServer(options =>
             {
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients);
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                {
+                    builder.UseSqlServer(connectionString, sql =>
+                    sql.MigrationsAssembly(migrationsAssembly));
+                };
+            })// this adds the operational data from DB (codes, tokens, consents)
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b =>
+                    b.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                // this enables automatic token cleanup. this is optional.
+                options.EnableTokenCleanup = true;
+            });
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
@@ -43,10 +64,12 @@ namespace SM.APIs.AuthServer
                 app.UseDeveloperExceptionPage();
             }
 
+            SeedData.EnsureSeedData(app);
+
             // uncomment if you want to add MVC
             //app.UseStaticFiles();
             //app.UseRouting();
-            
+
             app.UseIdentityServer();
 
             // uncomment, if you want to add MVC
