74e5b21610a50c9026d7fcd2686ed102d8cde022
SM <temp.smalldragon@gmail.com>
2020-12-05 15:33:32 +0700

    Revert migration files

--- SM.APIs.AuthServer/Data/Migrations/IdentityServer/ConfigurationDb/20201205082948_InitialIdentityServerConfigurationDbMigration.Designer.cs
+++ SM.APIs.AuthServer/Data/Migrations/IdentityServer/ConfigurationDb/20201205082948_InitialIdentityServerConfigurationDbMigration.Designer.cs
@@ -4,14 +4,16 @@ using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SM.APIs.AuthServer.Data.Migrations.IdentityServer.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201205082948_InitialIdentityServerConfigurationDbMigration")]
+    partial class InitialIdentityServerConfigurationDbMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SM.APIs.AuthServer/Data/Migrations/IdentityServer/ConfigurationDb/20201205082948_InitialIdentityServerConfigurationDbMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SM.APIs.AuthServer.Data.Migrations.IdentityServer.ConfigurationDb
+{
+    public partial class InitialIdentityServerConfigurationDbMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
